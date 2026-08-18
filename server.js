1d0e3f276b5f61c6f7f298e5bf24017bb60b38ec
mubarek jemal <mubarekjemal2736@gmail.com>
2026-08-18 12:22:27 +0300

    updated the home page

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 const { createServer } = require("http");
 const next = require("next");
+const { parse } = require("url");
 
-const dev = false;
-const hostname = "0.0.0.0";
-const port = parseInt(process.env.PORT || "3000", 10);
-
-const app = next({
-  dev,
-  hostname,
-  port,
-});
-
+const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    handle(req, res);
-  }).listen(port, hostname, () => {
-    console.log(
-      `> Ethiopia Maps running on http://${hostname}:${port}`
-    );
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
